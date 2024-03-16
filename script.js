2e8aad65a9595b04fab20d5af37259e4734994c8
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-03-16 22:18:14 +0200

    Add checkUserInput function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 const number = document.getElementById("number");
+
+const checkUserInput = () => {
+    
+}
