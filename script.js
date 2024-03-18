7604cfb33fc05342238268cce33f083cee0149c8
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-03-18 20:20:44 +0200

    Add new if statement to checkUserInput

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const checkUserInput = () => {
     if (!number.value || isNaN(numberInt)) {
         alert("Please enter a valid number");
     }
+
+    if (number.value < 0) {
+        alert("Please enter a number greater than or equal to 1");
+    }
 }
 
 convertBtn.addEventListener("click", checkUserInput);
