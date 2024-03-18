9231bda6fd57fc60aa63e8f9be904da598f81e8d
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-03-18 20:23:33 +0200

    Modify if statements

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ const checkUserInput = () => {
 
     if (!number.value || isNaN(numberInt)) {
         alert("Please enter a valid number");
+        return;
     }
 
     if (number.value < 0) {
         alert("Please enter a number greater than or equal to 1");
+        return;
     }
 }
 
