34d1a86ab04b67a7eee908dfd0e464daa26e7a9c
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-03-27 19:46:08 +0200

    Refactor checkUserInput function

--- script.js
+++ script.js
@@ -7,39 +7,14 @@ const checkUserInput = () => {
 
     if (!number.value || isNaN(numberInt)) {
         output.textContent = "Please enter a valid number";
-            } else if (number.value >= 4000 ) {
+        } else if (number.value >= 4000 ) {
                 output.textContent = "Please enter a number less than or equal to 3999";
-            } 
+        } else {
+            output.textContent = toRoman(number);
+        }
+}
 
-    // if (number.value < 0) {
-    //     alert("Please enter a number greater than or equal to 1");
-    //     return;
-    // }
-
-    // if (number.value === 9 ) {
-    //     alert("XI");
-    //     return;
-    // }
-
-    // if (number.value === 16 ) {
-    //     alert("XVI");
-    //     return;
-    // }
-
-    // if (number.value === 649 ) {
-    //     alert("DCXLIX");
-    //     return;
-    // }
-
-    // if (number.value === 1023 ) {
-    //     alert("MXXIII");
-    //     return;
-    // }
-
-    // if (number.value === 3999 ) {
-    //     alert("MMMCMXCIX");
-    //     return;
-    // }
+const toRoman = () => {
 
 }
 
