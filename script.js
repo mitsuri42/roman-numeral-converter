f45cbfc0cc72def2c54eb9cbe8834cda6354e8d6
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-03-29 10:02:44 +0200

    Add if statement to checkUserInput function

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const checkUserInput = () => {
 
   if (!number.value || isNaN(inputNumber)) {
     output.textContent = "Please enter a valid number";
+  } else if (number.value < 1) {
+    output.textContent = "Please enter a number greater than or equal to 1";
   } else if (number.value >= 4000) {
     output.textContent = "Please enter a number less than or equal to 3999";
   } else {
